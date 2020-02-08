0f9c696010ffaf42881292d31737b1cd239e6b92
Arief Luthfi <ariefluthfi799@gmail.com>
2020-02-08 19:16:44 +0700

    Update bash_to_zsh.rb

--- bash_to_zsh.rb
+++ bash_to_zsh.rb
@@ -9,10 +9,10 @@ arr_bash_history = bash_hist_file.split("\n")
 # puts arr_bash_history.inspect
 zsh_history_file = File.open(ZSH_HISTORY_PATH, 'a')
 
-waktu = Time.now.to_i
+time = Time.now.to_i
 arr_bash_history.each do |hist|
-  waktu += 1
-  zsh_history_file.write(": #{waktu}:0;#{hist}\n")
+  time += 1
+  zsh_history_file.write(": #{time}:0;#{hist}\n")
 end
 
 zsh_history_file.close
